c6058b31e133f61c899653d63f09e9dbb7712e65
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2021-12-29 03:35:49 +0000

    Fix the fact that snailfish addition is not commutative

--- 2021/haskell/day18/Part2.hs
+++ 2021/haskell/day18/Part2.hs
@@ -6,6 +6,6 @@ main :: IO ()
 main = do
   input <- getContents
   let numbers = map parseLine $ lines input
-  let pairs = [(x1, x2) | x1 <- numbers, x2 <- numbers, x1 < x2]
+  let pairs = [(x1, x2) | x1 <- numbers, x2 <- numbers, x1 /= x2]
   let totals = map (magnitude . parseList . reduce . (uncurry add)) pairs
   print $ maximum totals
