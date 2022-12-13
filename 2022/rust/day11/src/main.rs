513d1bbc89deb63e14477dcb7774089392fe2710
Gerald Benischke <11561320+beny23@users.noreply.github.com>
2022-12-13 16:23:00 +0000

    Rust day 11 - step 2

--- 2022/rust/day11/src/main.rs
+++ 2022/rust/day11/src/main.rs
@@ -1,17 +1,20 @@
 use std::str::FromStr;
 use std::io::prelude::*;
 use std::io;
-use std::ops::Fn;
 use std::rc::Rc;
 use std::cell::RefCell;
+use num::integer::lcm;
 
-type Op = Box<dyn Fn(u64) -> u64>;
-type Test = Box<dyn Fn(u64) -> bool>;
+enum Op {
+    Pow,
+    Add(u64),
+    Mul(u64)
+}
 
 struct Monkey {
     levels: Rc<RefCell<Vec<u64>>>,
     op: Op,
-    test: Test,
+    test: u64,
     if_true: usize,
     if_false: usize,
     counter: Rc<RefCell<u64>>
@@ -22,20 +25,13 @@ fn parse() -> Vec<Monkey> {
     let lines: Vec<String> = io::stdin().lock().lines().map(|l| l.unwrap()).collect();
     for group in lines.chunks(7) {
         let levels = group[1][18..].split(", ").map(|v| u64::from_str(v).unwrap()).collect::<Vec<_>>();
+        let test = u64::from_str(&group[3][21..]).unwrap();
         let op: Op = match group[2][23..].split_once(" ") {
-            Some(("*", "old")) => Box::new(|x| x * x),
-            Some(("+", v)) => {
-                let v = u64::from_str(v).unwrap();
-                Box::new(move |x| x + v)
-            },
-            Some(("*", v)) => {
-                let v = u64::from_str(v).unwrap();
-                Box::new(move |x| x * v)
-            },
+            Some(("*", "old")) => Op::Pow,
+            Some(("+", v)) => Op::Add(u64::from_str(v).unwrap()),
+            Some(("*", v)) => Op::Mul(u64::from_str(v).unwrap()),
             _ => panic!("Invalid op")
         };
-        let div = u64::from_str(&group[3][21..]).unwrap();
-        let test: Test = Box::new(move |x| x % div == 0);
         let if_true = usize::from_str(&group[4][29..]).unwrap();
         let if_false = usize::from_str(&group[5][30..]).unwrap();
         let monkey = Monkey {
@@ -52,27 +48,31 @@ fn parse() -> Vec<Monkey> {
 }
 
 fn main() {
-    step(3, 20);
-    //step(1, 10000);
+    // step(3, 20);
+    step(1, 10000);
 }
 
 fn step(div: u64, rounds: usize) {
     let monkeys = parse();
-    for round in 0..rounds {
-        println!("Round {}", round);
+    let lcm = monkeys.iter().map(|m| m.test).reduce(|a, b| lcm(a, b)).unwrap();
+    for _ in 0..rounds {
         for monkey in monkeys.iter() {
             let mut counter = 0;
             let mut levels = monkey.levels.borrow_mut();
             while levels.len() > 0 {
                 counter += 1;
                 let level = levels.remove(0);
-                let new_level = (monkey.op)(level) / div;
-                let next_monkey = if (monkey.test)(new_level) {
+                let new_level = match monkey.op {
+                    Op::Pow => level.pow(2),
+                    Op::Mul(v) => level * v,
+                    Op::Add(v) => level + v
+                } / div;
+                let next_monkey = if new_level % monkey.test == 0 {
                     monkey.if_true
                 } else {
                     monkey.if_false
                 };
-                monkeys[next_monkey].levels.borrow_mut().push(new_level);
+                monkeys[next_monkey].levels.borrow_mut().push(new_level % lcm);
             }
             *monkey.counter.borrow_mut() += counter;
         }
